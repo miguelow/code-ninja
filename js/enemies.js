821da260b6c921c8db6687ee2ad81c4c69785a61
VmMad <88061365+VmMad@users.noreply.github.com>
2022-01-23 19:08:43 +0100

    added computerjs

---
+++ js/enemies.js
@@ -0,0 +1,10 @@
+class Enemy {
+    constructor(ctx, enemyPosX, enemyPosY, enemyVelocity) {
+        this.ctx = ctx
+        this.enemyPos = { x: enemyPosX, y: enemyPosY }
+        this.enemyVelocity = enemyVelocity
+
+    }
+
+
+}
